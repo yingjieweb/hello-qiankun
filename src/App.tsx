900cb2af44eecf7338ae539d837d0621ba549266
yingjieweb <1184061078@qq.com>
2023-07-23 12:15:02 +0800

    style: 💄 adjust styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 import "./registerApps";
+import "./App.scss";
 
 const App: React.FC = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Link to="/vue2">Vue2</Link>
-        <Link to="/vue3">Vue3</Link>
-        <Link to="/react">React</Link>
-      </BrowserRouter>
+    <div className="baseContainer">
+      <div className="baseNav">
+        <BrowserRouter>
+          <Link to="/vue2">Vue2</Link>
+          <Link to="/vue3">Vue3</Link>
+          <Link to="/react">React</Link>
+        </BrowserRouter>
+      </div>
 
       {/* 切换导航，将微应用渲染到 container 容器中 */}
-      <div
-        id="container"
-        style={{
-          border: "1px solid red",
-          height: "100vh",
-          width: "100vw",
-        }}
-      ></div>
-    </>
+      <div id="microContainer"></div>
+    </div>
   );
 };
 
